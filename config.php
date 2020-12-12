d60666d029f76208c49b5a7c130c50e766c587c4
Akshaya <akshaya.swaroop3291@gmail.com>
2020-12-12 12:57:42 +0530

    version 1.0 added

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+if(!defined('MPG_MONERIS_PAYMENT_GATEWAY_PLUGIN_TOKEN')) {
+	define('MPG_MONERIS_PAYMENT_GATEWAY_PLUGIN_TOKEN', 'mpg-moneris-payment-gateway');
+}
+
+if(!defined('MPG_MONERIS_PAYMENT_GATEWAY_TEXT_DOMAIN')) {
+	define('MPG_MONERIS_PAYMENT_GATEWAY_TEXT_DOMAIN', 'wc_moneris_payment_gateway');
+}
+
+if(!defined('MPG_MONERIS_PAYMENT_GATEWAY_PLUGIN_VERSION')) {
+	define('MPG_MONERIS_PAYMENT_GATEWAY_PLUGIN_VERSION', '1.0.0');
+}
+
+if(!defined('MPG_MONERIS_PAYMENT_GATEWAY_PLUGIN_ICON')) {
+	define('MPG_MONERIS_PAYMENT_GATEWAY_PLUGIN_ICON', plugins_url( '/assets/images/logo.png', __FILE__ ));
+}
