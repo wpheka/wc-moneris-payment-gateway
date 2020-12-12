146ebc7e3ee06abc7699fdbcb726c783ad2073bd
Akshaya <akshaya.swaroop3291@gmail.com>
2020-12-12 13:00:35 +0530

    version 1.1 added

---
+++ classes/class-mpg-moneris-payment-gateway-init.php
@@ -0,0 +1,110 @@
+<?php
+class MPG_Moneris_Payment_Gateway_Init {
+
+	public $plugin_url;
+
+	public $plugin_path;
+
+	public $version;
+
+	public $token;
+
+	public $icon;
+	
+	public $text_domain;
+	
+	public $library;
+
+	public $shortcode;
+
+	public $admin;
+
+	public $frontend;
+
+	public $template;
+
+	public $ajax;
+
+	private $file;
+	
+	public $settings;
+	
+	public $dc_wp_fields;
+
+	public function __construct($file) {
+
+		$this->file = $file;
+		$this->plugin_url = trailingslashit(plugins_url('', $plugin = $file));
+		$this->plugin_path = trailingslashit(dirname($file));
+		$this->token = MPG_MONERIS_PAYMENT_GATEWAY_PLUGIN_TOKEN;
+		$this->text_domain = MPG_MONERIS_PAYMENT_GATEWAY_TEXT_DOMAIN;
+		$this->version = MPG_MONERIS_PAYMENT_GATEWAY_PLUGIN_VERSION;
+		$this->icon = MPG_MONERIS_PAYMENT_GATEWAY_PLUGIN_ICON;
+
+		add_action('init', array(&$this, 'load_plugin_textdomain'), 0);
+		add_action( 'plugins_loaded', array(&$this,'init_moneris_gateway_class') );
+		add_filter( 'woocommerce_payment_gateways', array(&$this,'mpg_add_moneris_payment_gateway') );
+		add_filter( 'plugin_action_links_' . plugin_basename($file), array($this, 'mpg_action_links' ) );
+	}
+
+	/**
+	 * Add Moneris Payment Gateway to woocommerce
+	 * @param $methods
+	 *
+	 * @return array
+	 */
+	function mpg_add_moneris_payment_gateway( $methods ) {
+		$methods[] = 'mpg_WOO_Moneris_Payment_Gateway';
+		return $methods;
+	}
+
+	/**
+	 * Add Plugin action links
+	 * @param $links
+	 *
+	 * @return array
+	 */
+	function mpg_action_links($links) {
+		$plugin_links = array(
+			'<a href="' . admin_url( 'admin.php?page=wc-settings&tab=checkout&section=moneris' ) . '">' . __( 'Settings', $this->text_domain ) . '</a>',
+		);
+		return array_merge( $plugin_links, $links );
+	}
+
+	/**
+	 * Moneris Gateway Class
+	 */
+	function init_moneris_gateway_class() {
+		if(class_exists('WC_Payment_Gateway')) {
+			$this->load_class('woo');
+			$this->admin = new mpg_WOO_Moneris_Payment_Gateway();
+		}
+	}
+
+	/**
+	 * Load Localisation files.
+	 *
+	 * Note: the first-loaded translation file overrides any following ones if the same translation is present
+	 *
+	 * @access public
+	 * @return void
+	 */
+	public function load_plugin_textdomain() {
+		$locale = apply_filters( 'plugin_locale', get_locale(), $this->token );
+
+		load_textdomain( $this->text_domain, WP_LANG_DIR . "/mpg-Moneris-Payment-Gateway/mpg-Moneris-Payment-Gateway-$locale.mo" );
+		load_textdomain( $this->text_domain, $this->plugin_path . "/languages/mpg-Moneris-Payment-Gateway-$locale.mo" );
+	}
+
+
+	/**
+	 * Plugin class loader
+	 * @param string $class_name
+	 */
+	public function load_class($class_name = '') {
+		if ('' != $class_name && '' != $this->token) {
+			require_once ('class-' . esc_attr($this->token) . '-' . esc_attr($class_name) . '.php');
+		} // End If Statement
+	}// End load_class()	
+
+}
---
+++ wc-moneris-payment-gateway.php
@@ -0,0 +1,39 @@
+<?php
+/*
+Plugin Name: WC Moneris Payment Gateway
+Plugin URI: https://github.com/AkshayaDev/wc-moneris-payment-gateway
+Description: WooCommerce Moneris Payment Gateway
+Author: Akshaya Swaroop
+Version: 1.1
+Author URI: https://github.com/AkshayaDev/
+Requires at least: 4.4
+Tested up to: 4.9.1
+Text Domain: wc_moneris_payment_gateway
+Domain Path: /languages/
+*/
+
+if ( ! class_exists( 'MPG_Dependencies' ) )
+	require_once trailingslashit(dirname(__FILE__)).'includes/class-mpg-dependencies.php';
+
+require_once trailingslashit(dirname(__FILE__)).'config.php';
+if(!defined('ABSPATH')) exit; // Exit if accessed directly
+if(!defined('MPG_MONERIS_PAYMENT_GATEWAY_PLUGIN_TOKEN')) exit;
+if(!defined('MPG_MONERIS_PAYMENT_GATEWAY_TEXT_DOMAIN')) exit;
+
+if(!class_exists('MPG_Moneris_Payment_Gateway') && MPG_Dependencies::is_woocommerce_active()) {
+	require_once( trailingslashit(dirname(__FILE__)).'classes/class-mpg-moneris-payment-gateway-init.php' );
+	global $MPG_Moneris_Payment_Gateway;
+	$MPG_Moneris_Payment_Gateway = new MPG_Moneris_Payment_Gateway_Init( __FILE__ );
+	$GLOBALS['MPG_Moneris_Payment_Gateway'] = $MPG_Moneris_Payment_Gateway;
+}else {
+	add_action('admin_notices', 'mpg_admin_notice');
+	if (!function_exists('mpg_admin_notice')) {
+		function mpg_admin_notice() {
+			?>
+			<div class="error">
+				<p><?php _e('Moneris Payment Gateway plugin requires <a href="http://wordpress.org/extend/plugins/woocommerce/">WooCommerce</a> plugin to be active!', MPG_MONERIS_PAYMENT_GATEWAY_TEXT_DOMAIN); ?></p>
+			</div>
+			<?php
+		}
+	}
+}
